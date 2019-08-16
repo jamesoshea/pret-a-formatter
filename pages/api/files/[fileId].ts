8514e09f312aa161e4570335598e35ad8cc595a3
James O'Shea <j.oshea@holidaypirates.com>
2019-08-16 15:08:42 +0200

    feat: use next api to fetch files from aws

--- pages/api/files/[fileId].ts
+++ pages/api/files/[fileId].ts
@@ -1,9 +1,11 @@
 import { S3GetObjectPromisified } from '../../../src/utils/promisified-functions'
 
+// example
+// http://localhost:3000/api/files/02816c183fb89f037fb0c00f451f309f-1563730623677-formatted
 export default async (req: any, res: any) => {
   try {
     const data = await S3GetObjectPromisified(
-      `${req.params.fileName.replace('-formatted', '')}.js`
+      `${req.query.fileId.replace('-formatted', '')}.js`
     )
     res.send(data.Body.toString())
   } catch (error) {
